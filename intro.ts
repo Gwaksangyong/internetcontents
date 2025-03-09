7d383875e85367fc800d9e1db6d8669025e9d7dd
ctmj4374 <ctmj4374@gmail.com>
2025-03-09 21:17:04 +0900

    Add files via upload

---
+++ intro.ts
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>처음입니다.</title>
+    </head>
+    <body>
+        <h1>곽상용 자기소개</h1>
+        <hr>
+        <p>
+            안녕하세요. 저는 25학번 곽상용입니다.
+         제가 가장 좋아하는 음식은 햄버거입니다.
+        </p>
+    </body>
+</html>
